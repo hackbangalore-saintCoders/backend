277d4c402b5603d36d1a25aa7aa3f976ba6c1f18
Mahesh Warang <76471375+maheshwarang001@users.noreply.github.com>
2024-05-10 15:31:55 +0530

    starter-package

---
+++ src/main/java/hackbangalore/freelancer/ai/FreelancerBackendMonolethicApplication.java
@@ -0,0 +1,13 @@
+package hackbangalore.freelancer.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerBackendMonolethicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelancerBackendMonolethicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hackbangalore/freelancer/ai/FreelancerBackendMonolethicApplicationTests.java
@@ -0,0 +1,13 @@
+package hackbangalore.freelancer.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerBackendMonolethicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
